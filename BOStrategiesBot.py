af5b565fbdf7f533275e21ad2ec76d139d7d1711
octaviodegodoy <octaviodegodoy@gmail.com>
2022-03-23 00:39:52 -0300

    desenvolvendo soros gale

--- BOStrategiesBot.py
+++ BOStrategiesBot.py
@@ -14,71 +14,7 @@ from iqoptionapi.stable_api import IQ_Option
 from datetime import datetime
 import time
 import sys
-import math
 import configparser
-
-perda_acumulada = 0
-
-
-def get_all_opened_assets():
-    return API.get_all_open_time()
-
-
-def is_asset_open(asset, all_opened_assets, mode='turbo'):
-    return all_opened_assets[mode][asset]["open"]
-
-
-def get_digital_profit(par, expiration):
-    API.subscribe_strike_list(par, expiration)
-    payout = API.get_digital_current_profit(par, expiration)
-    while not payout:
-        time.sleep(0.1)
-        payout = API.get_digital_current_profit(par, expiration)
-    API.unsubscribe_strike_list(par, expiration)
-    return {'digital': math.floor(payout) / 100}
-
-
-def get_all_profits():
-    return API.get_all_profit()
-
-
-def most_profit_mode(par, expiration, min_payout):
-    _mpm = ['digital', False]
-
-    all_opened_assets = get_all_opened_assets()
-    opened = dict()
-    for mode in ['turbo', 'digital']:
-        opened[mode] = is_asset_open(par, all_opened_assets, mode)
-    if opened['turbo'] or opened['digital']:
-        profits = get_all_profits()
-        if opened['digital']:
-            if par in profits:
-                profits[par].update(get_digital_profit(par, expiration))
-            else:
-                profits[par] = get_digital_profit(par, expiration)
-        priority_mode_list = []
-        for k, v in opened.items():
-            if v:
-                priority_mode_list.append([k, profits[par][k]])
-        priority_mode_list = sorted(priority_mode_list, key=lambda x: x[1], reverse=True)
-        if priority_mode_list:
-            mode, best_payout = priority_mode_list[0]
-            if best_payout >= min_payout:
-                if mode == 'turbo':
-                    _mpm[0], _mpm[1] = 'turbo', True
-                else:
-                    _mpm[0], _mpm[1] = 'digital', True
-            else:
-                # print(str(datetime.now()), "The payout for " + active + " is below " + str(float(best_payout) * 100) + "%")
-                _mpm[0], _mpm[1] = 'payout', False
-        else:
-            # print(str(datetime.now()), active, "- Something went wrong. No items in your priority list :(")
-            _mpm[0], _mpm[1] = 'error', False
-    else:
-        # print(str(datetime.now()), active + " is closed now. :(")
-        _mpm[0], _mpm[1] = 'closed', False
-
-    return _mpm[0], _mpm[1]
 
 
 def remaining_seconds(minutes):
@@ -112,15 +48,6 @@ def entradas(par, entrada, direcao, operacao):
         return False, 0
 
 
-def soros_gale(valor, entrada):
-    if valor < 0:
-        perda_acumulada = abs(valor)
-        perda_acumulada += perda_acumulada / 2
-        return perda_acumulada
-    elif valor > 0:
-        return entrada + valor
-
-
 def payout(par):
     API.subscribe_strike_list(par, 1)
     while True:
@@ -151,10 +78,10 @@ def get_balance():
     return API.get_balance()
 
 
-def get_initial_amount(payout, amount_by_payout):
-    balance = get_balance()
-    initial_percent = float(amount_by_payout[payout]) / 100
-    return round(initial_percent * balance, 2)
+def get_initial_amount(par, amount_by_payout):
+    payout = get_payout(par)
+    initial_percent = float(amount_by_payout[str(payout)]) / 100
+    return round(initial_percent * capital_inicial, 2)
 
 
 def configure():
@@ -222,11 +149,13 @@ tipo_mhi = int(1)  # int(input(' Deseja operar a favor da\n  1 - Minoria\n  2 - 
 par = 'EURUSD'  # input(' Indique uma paridade para operar: ').upper()
 martingale = int(12)  # int(input(' Indique a quantia de martingales: '))
 martingale += 1
+capital_inicial = float(20000.0)
 
-stop_loss = float(1100.0)  # float(input(' Indique o valor de Stop Loss: '))
-stop_gain = float(700.0)  # float(input(' Indique o valor de Stop Gain: '))
+meta_diaria_ganho = float(2/100)*capital_inicial
+meta_diaria_risco = float(3/100)*capital_inicial
 
-# payout = payout(par)
+stop_loss = meta_diaria_risco # float(input(' Indique o valor de Stop Loss: '))
+stop_gain = meta_diaria_ganho  # float(input(' Indique o valor de Stop Gain: '))
 
 amount_by_payout = {'0.74': '0.99', '0.75': '0.97', '0.76': '0.96', '0.77': '0.94', '0.78': '0.93', '0.79': '0.91',
                     '0.80': '0.90', '0.81': '0.88', '0.82': '0.87', '0.83': '0.85', '0.84': '0.84', '0.85': '0.83',
@@ -234,10 +163,13 @@ amount_by_payout = {'0.74': '0.99', '0.75': '0.97', '0.76': '0.96', '0.77': '0.9
                     '0.92': '0.75', '0.93': '0.74', '0.94': '0.73', '0.95': '0.72', '0.96': '0.71', '0.97': '0.70',
                     '0.98': '0.69', '0.99': '0.68', '100': '0.67'}
 
-valor_entrada = float(5.0)
+
+valor_entrada = get_initial_amount(par, amount_by_payout)
+
 perda_acumulada = 0
 lucro = 0
 valor_soros = 0
+lucro = 0
 win_count = 0
 while True:
     minutos = 1
@@ -254,16 +186,32 @@ while True:
             print('Entrando com :', direcao)
 
             status, valor = entradas(par, valor_soros + valor_entrada, direcao, operacao)
-            #valor = -5
+            lucro += valor
 
-            if valor < 0:
+            if 0 < perda_acumulada < valor:
+                valor_entrada = get_initial_amount(par, amount_by_payout)
+                continue
+
+            if valor < 0 and win_count == 0:
                 valor_entrada = 0
                 perda_acumulada += abs(valor)
-                valor_soros = perda_acumulada / 2
-            elif valor > 0:
-                valor_entrada += valor_soros + valor
+                valor_soros = float(perda_acumulada) / 2
+            elif valor < 0 and win_count == 1:
+                valor_entrada = 0
+                perda_acumulada = abs(lucro)
+                valor_soros = float(perda_acumulada) / 2
+                win_count = 0
+            elif valor > 0 and win_count == 0:
                 win_count += 1
+                valor_entrada += round(valor_soros + valor, 2)
+                valor_soros = 0
+            elif valor > 0 and win_count > 0:
+                valor_entrada = get_initial_amount(par, amount_by_payout)
+                valor_soros = 0
+                continue
 
+            stop(lucro, stop_gain, stop_loss)
+            capital_inicial += lucro
             # time.sleep(minutos*60)
             """
             for i in range(martingale):
